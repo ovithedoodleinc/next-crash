294a9d5ccb890ffcc88142b2b8a08d90c2e3af03
Showvike Mondal Ovi <37304812+showvike@users.noreply.github.com>
2024-01-30 01:57:52 +0600

    static meta done

--- app/about/layout.js
+++ app/about/layout.js
@@ -1,5 +1,10 @@
 import Link from "next/link";
 
+export const metadata = {
+  title: "About",
+  description: "This is about page",
+};
+
 export default function AboutLayout({ children }) {
   const routes = [
     { name: "Mission", path: "/about/mission" },
