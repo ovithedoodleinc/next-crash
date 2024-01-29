46ed0dbed628fd6b036c5f4d234b91577b69ebdb
Showvike Mondal Ovi <37304812+showvike@users.noreply.github.com>
2024-01-29 22:47:25 +0600

    optimize image with image component

--- app/about/mission/page.js
+++ app/about/mission/page.js
@@ -1,9 +1,14 @@
 import Button from "@/components/button";
+import thumb from "@/public/thumb.jpg";
+import Image from "next/image";
 
 export default function Mission() {
   return (
     <main className="mt-10">
       <div>Mission Page</div>
+      <div className="w-96">
+        <Image src={thumb} alt="thumb" placeholder="blur" />
+      </div>
       <Button />
     </main>
   );
